1094a741deb038aa320519bc19d03c136ed3fd31
Eric Chau <eriic.chau@gmail.com>
2017-06-27 21:49:45 +0000

    added event receiver to run event to set request content into request if needed

--- src/RestCore.php
+++ src/RestCore.php
@@ -8,6 +8,7 @@ use Jarvis\Jarvis;
 use Jarvis\Skill\DependencyInjection\ContainerProviderInterface;
 use Jarvis\Skill\EventBroadcaster\BroadcasterInterface;
 use Jarvis\Skill\EventBroadcaster\ExceptionEvent;
+use Jarvis\Skill\EventBroadcaster\RunEvent;
 use Jarvis\Skill\Rest\Annotation\CriteriaHandler;
 use Jarvis\Skill\Rest\Annotation\PaginationHandler;
 use Jarvis\Skill\Rest\Annotation\SortHandler;
@@ -25,6 +26,16 @@ class RestCore implements ContainerProviderInterface
      */
     public function hydrate(Jarvis $app)
     {
+        $this
+            ->mountRestServices($app)
+            ->mountRestEventReceivers($app)
+        ;
+    }
+
+    protected function mountRestServices(Jarvis $app)
+    {
+        $app['request_content_json_only'] = false;
+
         $request = $app['request'];
         $app['annotation.handler.rest.pagination'] = function () use ($request): PaginationHandler {
             return new PaginationHandler($request);
@@ -37,6 +48,31 @@ class RestCore implements ContainerProviderInterface
         $app['annotation.handler.rest.criteria'] = function () use ($request): CriteriaHandler {
             return new CriteriaHandler($request);
         };
+
+        return $this;
+    }
+
+    protected function mountRestEventReceivers(Jarvis $app)
+    {
+        $app->on(BroadcasterInterface::RUN_EVENT, function (RunEvent $event) use ($app): void {
+            $request = $event->request();
+            $contentType = $request->getContentType();
+            if (false === $app['request_content_json_only'] && 'json' !== $contentType) {
+                return;
+            }
+
+            $request->request->replace();
+            if (in_array($request->getMethod(), ['GET', 'HEAD']) || 'json' !== $contentType) {
+                return;
+            }
+
+            $content = json_decode($request->getContent(), true);
+            if (JSON_ERROR_NONE !== json_last_error() || !is_array($content)) {
+                return;
+            }
+
+            $request->request->replace($content);
+        });
 
         $app->on(BroadcasterInterface::EXCEPTION_EVENT, function (ExceptionEvent $event) {
             $exception = $event->exception();
